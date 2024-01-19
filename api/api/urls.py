c54c8b73c5cf0571e252f892221ec4cf8fcde3e1
lenrich <lenrich1403@gmail.com>
2024-01-19 12:19:42 +0800

    setting django restful framework api route

--- api/api/urls.py
+++ api/api/urls.py
@@ -18,7 +18,11 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'BenzEshop Admin'
+admin.site.index_title = 'Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('store/', include('store.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
